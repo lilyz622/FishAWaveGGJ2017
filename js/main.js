4090a492b45ac766ce165a50b4134e7eb15b77a2
willstreet <william.alexander13@stjohns.edu>
2017-01-21 14:39:22 -0500

    js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,9 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'Fish A Wave', { preload: preload, create: create, update: update });
 
 
 function preload(){
 	
-	game.load.image
-	
-	
-	game.load.audio
+
 	
 	
 }
