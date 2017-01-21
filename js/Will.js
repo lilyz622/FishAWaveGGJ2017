238712828bda7814fadfb2b5663b2e31aeb004e0
willstreet <william.alexander13@stjohns.edu>
2017-01-21 14:41:34 -0500

    load

--- js/Will.js
+++ js/Will.js
@@ -1,36 +1,10 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
-
-function preload(){
-	
-	
-	
+function preload() {
 }
-
-var stuff;
-
-
 
 function create() {
-	
-	
-	
-	
-	
 }
-
-
 
 function update() {
-	
-	
-	
-	
-}
-
-function render() {
-	
-	game.debug.text("Time until event: " + game.time.events.duration.toFixed(0), 32, 32);
-    game.debug.text("Next tick: " + game.time.events.next.toFixed(0), 32, 64);
-	
 }
