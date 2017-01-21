5d9e1f816a94b47cc18b390675a91f5cc311d524
willstreet <william.alexander13@stjohns.edu>
2017-01-21 16:18:40 -0500

    fix

--- js/main.js
+++ js/main.js
@@ -58,7 +58,7 @@ function create() {
    
 
     // The player and its settings
-    player = game.add.sprite(32, game.world.height - 150, 'captain');
+    player = game.add.sprite(32, game.world.height - 400, 'captain');
 
     //  We need to enable physics on the player
     game.physics.arcade.enable(player);
@@ -101,35 +101,7 @@ function update() {
 	//  Reset the players velocity (movement)
     player.body.velocity.x = 0;
 	
-	if (cursors.left.isDown)
-    {
-        //  Move to the left
-        player.body.velocity.x = -150;
 
-        player.animations.play('left');
-		
-    }
-    else if (cursors.right.isDown)
-    {
-        //  Move to the right
-        player.body.velocity.x = 150;
-
-        player.animations.play('right');
-	
-    }
-    else
-    {
-        //  Stand still
-        player.animations.stop();
-
-        player.frame = 1;
-    }
-    
-    //  Allow the player to jump if they are touching the ground.
-    if (cursors.up.isDown && player.body.touching.down)
-    {
-        player.body.velocity.y = -350;
-    }
 	
 	
 	
