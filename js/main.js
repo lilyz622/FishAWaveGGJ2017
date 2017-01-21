1d7431077e715332d10add1fbb2183b34fc4975d
willstreet <william.alexander13@stjohns.edu>
2017-01-21 16:14:13 -0500

    movement

--- js/main.js
+++ js/main.js
@@ -101,6 +101,36 @@ function update() {
 	//  Reset the players velocity (movement)
     player.body.velocity.x = 0;
 	
+	if (cursors.left.isDown)
+    {
+        //  Move to the left
+        player.body.velocity.x = -150;
+
+        player.animations.play('left');
+		school.tilePosition.x+= 5;
+    }
+    else if (cursors.right.isDown)
+    {
+        //  Move to the right
+        player.body.velocity.x = 150;
+
+        player.animations.play('right');
+		school.tilePosition.x-=5;
+    }
+    else
+    {
+        //  Stand still
+        player.animations.stop();
+
+        player.frame = 4;
+    }
+    
+    //  Allow the player to jump if they are touching the ground.
+    if (cursors.up.isDown && player.body.touching.down)
+    {
+        player.body.velocity.y = -350;
+    }
+	
 	
 	
 }
