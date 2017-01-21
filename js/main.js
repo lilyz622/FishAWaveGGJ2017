e7c8f34f884bdf5469e947c15541e7cdac72492f
willstreet <william.alexander13@stjohns.edu>
2017-01-21 14:46:36 -0500

    js cleanup

--- js/main.js
+++ js/main.js
@@ -23,60 +23,60 @@ var fishText;
 function create() {
 	
 	  //add sound
-	game.input.touch.preventDefault = false;
-	sound = game.add.audio('music');
-	sound.play();
-	game.input.onDown.add(restartMusic, this);
+	//game.input.touch.preventDefault = false;
+	//sound = game.add.audio('music');
+	//sound.play();
+	//game.input.onDown.add(restartMusic, this);
 	
 	//  We're going to be using physics, so enable the Arcade Physics system
-    game.physics.startSystem(Phaser.Physics.ARCADE);
+    //game.physics.startSystem(Phaser.Physics.ARCADE);
 
     //  A simple background for our game
-    game.add.sprite(0, 0, 'sky');
+   // game.add.sprite(0, 0, 'sky');
 	//school = game.add.tileSprite(0, 0, 800, 600, 'clouds');
 
     //  The platforms group contains the ground and waves we are sailing on
-    platforms = game.add.group();
+    //platforms = game.add.group();
 
     //  We will enable physics for any object that is created in this group
-    platforms.enableBody = true;
+   // platforms.enableBody = true;
 
     // Here we create the ground.
-    var ground = platforms.create(0, game.world.height - 64, 'ground');
+   // var ground = platforms.create(0, game.world.height - 64, 'ground');
 
     //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
-    ground.scale.setTo(2, 2);
+    //ground.scale.setTo(2, 2);
 
     //  This stops it from falling away when you jump on it
-    ground.body.immovable = true;
+   // ground.body.immovable = true;
 
     //  The wave form
     //***Playing with beizer curves
 
     // The player and its settings
-    player = game.add.sprite(32, game.world.height - 150, 'captain');
+  //  player = game.add.sprite(32, game.world.height - 150, 'captain');
 
     //  We need to enable physics on the player
-    game.physics.arcade.enable(player);
+   // game.physics.arcade.enable(player);
 
     //  Player physics properties. No bounce for the ship.
-    player.body.bounce.y = 0;
-    player.body.gravity.y = 400;
-    player.body.collideWorldBounds = true;
+   // player.body.bounce.y = 0;
+  //  player.body.gravity.y = 400;
+  //  player.body.collideWorldBounds = true;
 
     //  Our two animations, sailing left and right.
-    player.animations.add('left', [1, 5, 9, 13], 16, true);
-	player.animations.add('right', [3, 7, 11, 15], 16, true);
+  //  player.animations.add('left', [1, 5, 9, 13], 16, true);
+	//player.animations.add('right', [3, 7, 11, 15], 16, true);
 
    
 
     //  The score
-    scoreText = game.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#000' });
+    //scoreText = game.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#000' });
 	//The fish count
-	fishText = game.add.text(16, 32, 'Fish: 0', { fontSize: '32px', fill: '#000' });
+	//fishText = game.add.text(16, 32, 'Fish: 0', { fontSize: '32px', fill: '#000' });
 
     //  Our controls.
-    cursors = game.input.keyboard.createCursorKeys();
+    //cursors = game.input.keyboard.createCursorKeys();
 	
 	
 	//  The first parameter is how long to wait before the event fires. In this case 5 seconds (you could pass in 2000 as the value as well.)
@@ -98,7 +98,7 @@ function update() {
 
 function render() {
 	
-	game.debug.text("Time until event: " + game.time.events.duration.toFixed(0), 32, 32);
-    game.debug.text("Next tick: " + game.time.events.next.toFixed(0), 32, 64);
+	//game.debug.text("Time until event: " + game.time.events.duration.toFixed(0), 32, 32);
+    //game.debug.text("Next tick: " + game.time.events.next.toFixed(0), 32, 64);
 	
 }
